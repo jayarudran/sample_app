cf0a45c13b34e736280a00cbe9bace8dd21ec659
Ubuntu <ubuntu@ip-172-31-25-178.us-east-2.compute.internal>
2020-07-13 17:41:10 +0000

    Implement Advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,10 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   # Add more helper methods to be used by all tests here...
+  
+  
+  # Log in as a particular user.
+  def log_in_as(user) 
+    session[:user_id] = user.id
+  end
 end
